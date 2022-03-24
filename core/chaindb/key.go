5e6c9b90c5a81892503c8aaa8bdbdf09dd3cb05d
luxq <xueqian1991@gmail.com>
2022-03-24 12:25:28 +0800

    update for key gen.

--- core/chaindb/key.go
+++ core/chaindb/key.go
@@ -3,7 +3,6 @@ package chaindb
 import (
 	"github.com/hashrs/consensusdemo/types"
 	"math/big"
-	"strings"
 )
 
 const (
@@ -14,31 +13,35 @@ const (
 )
 
 func chainHeightKey() string {
-	return "hei-:chain"
+	return "hei-chain"
 }
 
 func blockKey(number *big.Int) string {
-	var builder strings.Builder
-	builder.WriteString(BlockKeyPrefix)
-	builder.WriteString(":")
-	builder.WriteString(number.Text(10))
-
-	return builder.String()
+	n := number.Text(10)
+	s := make([]byte, len(n)+len(BlockKeyPrefix))
+	copy(s, BlockKeyPrefix)
+	copy(s[len(BlockKeyPrefix):], n)
+	//s = append(s, BlockKeyPrefix...)
+	//s = append(s, n...)
+	return string(s)
 }
 
 func transactionKey(hash types.Hash) string {
-	var builder strings.Builder
-	builder.WriteString(TransactionKeyPrefix)
-	builder.WriteString(":")
-	builder.WriteString(hash.String())
-
-	return builder.String()
+	h := hash.String()
+	s := make([]byte, len(h)+len(TransactionKeyPrefix))
+	copy(s, TransactionKeyPrefix)
+	copy(s[len(TransactionKeyPrefix):], h)
+	//s = append(s, TransactionKeyPrefix...)
+	//s = append(s, h...)
+	return string(s)
 }
 
 func receiptKey(hash types.Hash) string {
-	var builder strings.Builder
-	builder.WriteString(ReceiptKeyPrefix)
-	builder.WriteString(":")
-	builder.WriteString(hash.String())
-	return builder.String()
+	h := hash.String()
+	s := make([]byte, len(h)+len(ReceiptKeyPrefix))
+	copy(s, ReceiptKeyPrefix)
+	copy(s[len(ReceiptKeyPrefix):], h)
+	//s = append(s, ReceiptKeyPrefix...)
+	//s = append(s, h...)
+	return string(s)
 }
